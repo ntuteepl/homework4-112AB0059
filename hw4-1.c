b25872cfc77b91d124fc3b095aee31e177e0aaca
112AB0059 <146065279+112AB0059@users.noreply.github.com>
2023-10-31 10:49:58 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    if (num <= 3) {
+        return 1;
+    }
+    if (num % 2 == 0 || num % 3 == 0) {
+        return 0;
+    }
+
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main() {
+    int n;
+    printf("請輸入n：\n");
+    scanf("%d", &n);
+
+    printf("請依序輸入 %d 個正整數：\n", n);
+    for (int i = 0; i < n; i++) {
+        int num;
+        scanf("%d", &num);
+        if (isPrime(num)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
